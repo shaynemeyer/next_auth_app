e614d49ab4b116d0454cb82de87fa70c9a73782a
Shayne Meyer <shayne.meyer@gmail.com>
2024-09-17 07:03:55 -0700

    added qrcode renderer

--- components/TwoFactorAuth/TwoFactorAuthForm.tsx
+++ components/TwoFactorAuth/TwoFactorAuthForm.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { get2faSecret } from "./actions";
+import { useToast } from "@/hooks/use-toast";
+import { QRCodeSVG } from "qrcode.react";
 
 function TwoFactorAuthForm({
   twoFactorActivated,
@@ -10,9 +13,23 @@ function TwoFactorAuthForm({
 }) {
   const [isActivated, setIsActivated] = useState(twoFactorActivated);
   const [step, setStep] = useState(1);
+  const [code, setCode] = useState("");
+  const { toast } = useToast();
 
-  const handleEnableClick = () => {
+  const handleEnableClick = async () => {
+    const response = await get2faSecret();
+
+    if (response?.error) {
+      toast({
+        variant: "destructive",
+        title: response.message,
+      });
+
+      return;
+    }
+
     setStep(2);
+    setCode(response.twoFactorSecret ?? "");
   };
   return (
     <div>
@@ -23,7 +40,25 @@ function TwoFactorAuthForm({
               Enable Two-Factor Authentication
             </Button>
           )}
-          {step === 2 && <div>display qr code</div>}
+          {step === 2 && (
+            <div>
+              <p className="text-xs text-muted-foreground py-2">
+                Scan the QR code below in the Google Authenticator app to
+                activate Two-Factor Authentication.
+              </p>
+              <QRCodeSVG value={code} />
+              <Button onClick={() => setStep(3)} className="w-full my-2">
+                I have scanned the QR code
+              </Button>
+              <Button
+                variant="outline"
+                onClick={() => setStep(1)}
+                className="w-full my-2"
+              >
+                Cancel
+              </Button>
+            </div>
+          )}
         </div>
       )}
     </div>
